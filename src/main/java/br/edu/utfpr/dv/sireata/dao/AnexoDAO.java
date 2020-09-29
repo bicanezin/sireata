705c45d61fc0d4b838c40f2c890b927f8dfa34a7
Bianca Canezin <bcanezin@alunos.utfpr.edu.br>
2020-09-29 13:58:09 -0300

    Aplica try-with-resources na classe AnexoDAO

--- src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoDAO.java
+++ src/main/java/br/edu/utfpr/dv/sireata/dao/AnexoDAO.java
@@ -36,49 +36,46 @@ public class AnexoDAO extends CommonMethods<Anexo> {
     }
 
     @Override
-    public int salvar(Anexo anexo) throws SQLException {
-        boolean insert = (anexo.getIdAnexo() == 0);
-        Connection conn = null;
-        PreparedStatement stmt = null;
-        ResultSet rs = null;
-
-        try {
-            conn = ConnectionDAO.getInstance().getConnection();
-
-            if (insert) {
-                stmt = conn.prepareStatement("INSERT INTO anexos(idAta, ordem, descricao, arquivo) VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
-            } else {
-                stmt = conn.prepareStatement("UPDATE anexos SET idAta=?, ordem=?, descricao=?, arquivo=? WHERE idAnexo=?");
-            }
-
+    public int inserir(Anexo anexo) throws SQLException {
+        try (Connection conn = ConnectionDAO.getInstance().getConnection();
+             PreparedStatement stmt = conn.prepareStatement("INSERT INTO anexos(idAta, ordem, descricao, arquivo) VALUES(?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS)
+        ) {
             stmt.setInt(1, anexo.getAta().getIdAta());
             stmt.setInt(2, anexo.getOrdem());
             stmt.setString(3, anexo.getDescricao());
             stmt.setBytes(4, anexo.getArquivo());
 
-            if (!insert) {
-                stmt.setInt(5, anexo.getIdAnexo());
-            }
-
             stmt.execute();
 
-            if (insert) {
-                rs = stmt.getGeneratedKeys();
-
-                if (rs.next()) {
-                    anexo.setIdAnexo(rs.getInt(1));
-                }
+            try (ResultSet rs = stmt.getGeneratedKeys()) {
+                if (rs.next()) anexo.setIdAnexo(rs.getInt(1));
             }
+            return anexo.getIdAnexo();
+        }
+    }
+
+    @Override
+    public int atualizar(Anexo anexo) throws SQLException {
+        try (Connection conn = ConnectionDAO.getInstance().getConnection();
+             PreparedStatement stmt = conn.prepareStatement("UPDATE anexos SET idAta=?, ordem=?, descricao=?, arquivo=? WHERE idAnexo=?")
+        ) {
+            stmt.setInt(1, anexo.getAta().getIdAta());
+            stmt.setInt(2, anexo.getOrdem());
+            stmt.setString(3, anexo.getDescricao());
+            stmt.setBytes(4, anexo.getArquivo());
+
+            stmt.setInt(5, anexo.getIdAnexo());
+            stmt.execute();
 
             return anexo.getIdAnexo();
-        } finally {
-            if ((rs != null) && !rs.isClosed())
-                rs.close();
-            if ((stmt != null) && !stmt.isClosed())
-                stmt.close();
-            if ((conn != null) && !conn.isClosed())
-                conn.close();
         }
+    }
+
+    @Override
+    public int salvar(Anexo anexo) throws SQLException {
+        boolean inserir = (anexo.getIdAnexo() == 0);
+
+        return inserir ? inserir(anexo) : atualizar(anexo);
     }
 
     @Override
